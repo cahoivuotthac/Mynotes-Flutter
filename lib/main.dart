ee3c948604e4bffb7c9ca75887334369053b26c7
cahoivuotthac <22520423@gm.uit.edu.vn>
2023-09-11 23:47:52 +0700

    add popup menu

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ import 'firebase_options.dart';
 //import 'package:firebase_auth/firebase_auth.dart'; => for FirebaseAuth.instance.createUserWithEmailAndPassword function
 //import 'firebase_options.dart'; // also for Firebase.initializeApp function
 //import 'package:firebase_core/firebase_core.dart'; => for Firebase.initializeApp function
+import 'dart:developer' as devtools
+    show
+        log; //when you want to use a specific thing in this package, if not have show then will show everything in this package
 
 void main() {
   WidgetsFlutterBinding
@@ -53,16 +56,13 @@ class HomePage extends StatelessWidget {
                 .currentUser; //to storage credential information about the current user in Firebase Authentication
             if (user != null) {
               if (user.emailVerified) {
-                print('Email is verified');
+                return const NotesView();
               } else {
                 return const VerifyEmailView();
               }
             } else {
               return const LoginView();
             }
-            return const Text(
-                'Done!'); //if app has a user and user email is verified, he/she will see a text: Done
-
           //LoginView is embeded in HomePageView
 
           //if you tap in user, you can see that its type is nullable
@@ -80,4 +80,78 @@ class HomePage extends StatelessWidget {
       },
     );
   }
+}
+
+enum MenuAction {
+  logout
+} //enumeration called MenuAtion with a single value - logout
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Main UI'),
+          actions: [
+            //actions is list type, use [] for the type list
+            PopupMenuButton<MenuAction>(//display a popup menu when tapped icon
+                onSelected: (value) async {
+              //this will be called when a menu item is selected
+              //it takes the selected value as a parameter and performs an action based on the value
+              switch (value) {
+                case MenuAction
+                      .logout: //if the selected value is MenuAction.logout it shows a logout dialog
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login/', (route) => false);
+                  }
+              }
+            }, itemBuilder: (context) {
+              //create the menu items for the popup menu
+              //itemBuilder is list type
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout, //value is what programmer see
+                  child: Text('Log out'), //child is what user see
+                )
+              ];
+            })
+          ],
+        ),
+        body: const Text('Hello world'));
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      //create an alert dialogue
+      return AlertDialog(
+          title: const Text('Sign out'),
+          content: const Text('Are you sure you want to log out?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pop(false); //close the current dialog and return false
+                },
+                child: const Text('Cancel')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Log out'))
+          ]);
+    },
+  ).then((value) => value ?? false); //when users dismiss the dialog
 }
